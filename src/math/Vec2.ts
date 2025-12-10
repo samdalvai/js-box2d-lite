f69bb6dc1e5a7a111027c2ffff688f40d67e5d18
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-10 10:52:32 +0100

    refactor: move Vec2 static methods to Vec2 class

--- src/math/Vec2.ts
+++ src/math/Vec2.ts
@@ -33,4 +33,54 @@ export default class Vec2 {
     length = (): number => {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     };
+
+    static dot = (a: Vec2, b: Vec2): number => {
+        return a.x * b.x + a.y * b.y;
+    };
+
+    /** Scalar 2D cross product */
+    static cross = (a: Vec2, b: Vec2): number => {
+        return a.x * b.y - a.y * b.x;
+    };
 }
+
+/*
+// Vector perpendicular to a, scaled
+// This is the vector in the +90° (counterclockwise) perpendicular direction.
+inline float Cross(const Vec2& a, const Vec2& b)
+{
+	return a.x * b.y - a.y * b.x;
+}
+
+// Vector perpendicular to a, scaled in the opposite direction
+// This is the vector in the –90° (clockwise) perpendicular direction.
+inline Vec2 Cross(const Vec2& a, float s)
+{
+	return Vec2(s * a.y, -s * a.x);
+}
+
+inline Vec2 Cross(float s, const Vec2& a)
+{
+	return Vec2(-s * a.y, s * a.x);
+}
+
+inline Vec2 operator * (const Mat22& A, const Vec2& v)
+{
+	return Vec2(A.col1.x * v.x + A.col2.x * v.y, A.col1.y * v.x + A.col2.y * v.y);
+}
+
+inline Vec2 operator + (const Vec2& a, const Vec2& b)
+{
+	return Vec2(a.x + b.x, a.y + b.y);
+}
+
+inline Vec2 operator - (const Vec2& a, const Vec2& b)
+{
+	return Vec2(a.x - b.x, a.y - b.y);
+}
+
+inline Vec2 operator * (float s, const Vec2& v)
+{
+	return Vec2(s * v.x, s * v.y);
+}
+*/
