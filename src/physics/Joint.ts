ee703cf67b43f8dee50ebe8d4254221b07c26913
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-11 22:08:40 +0100

    feat: add implementation of Joint preStep method

--- src/physics/Joint.ts
+++ src/physics/Joint.ts
@@ -50,8 +50,67 @@ export default class Joint {
         this.biasFactor = 0.2;
     };
 
-    preStep = (invDt: number): void => {
-        //
+    // TODO: remove positionCorrection and warmStarting
+    preStep = (invDt: number, positionCorrection = true, warmStarting = true): void => {
+        if (!this.body1 || !this.body2) {
+            throw new Error('One or more bodies not initialized in Joint');
+        }
+
+        // Pre-compute anchors, mass matrix, and bias.
+        const Rot1 = new Mat22(this.body1.rotation);
+        const Rot2 = new Mat22(this.body2.rotation);
+
+        this.r1 = Mat22.multiply(Rot1, this.localAnchor1);
+        this.r2 = Mat22.multiply(Rot2, this.localAnchor2);
+
+        // deltaV = deltaV0 + K * impulse
+        // invM = [(1/m1 + 1/m2) * eye(2) - skew(r1) * invI1 * skew(r1) - skew(r2) * invI2 * skew(r2)]
+        //      = [1/m1+1/m2     0    ] + invI1 * [r1.y*r1.y -r1.x*r1.y] + invI2 * [r1.y*r1.y -r1.x*r1.y]
+        //        [    0     1/m1+1/m2]           [-r1.x*r1.y r1.x*r1.x]           [-r1.x*r1.y r1.x*r1.x]
+        const K1 = new Mat22();
+        K1.col1.x = this.body1.invMass + this.body2.invMass;
+        K1.col1.y = 0.0;
+        K1.col2.x = 0.0;
+        K1.col2.y = this.body1.invMass + this.body2.invMass;
+
+        const K2 = new Mat22();
+        K2.col1.x = this.body1.invI * this.r1.y * this.r1.y;
+        K2.col1.y = -this.body1.invI * this.r1.x * this.r1.y;
+        K2.col2.x = -this.body1.invI * this.r1.x * this.r1.y;
+        K2.col2.y = this.body1.invI * this.r1.x * this.r1.x;
+
+        const K3 = new Mat22();
+        K3.col1.x = this.body2.invI * this.r2.y * this.r2.y;
+        K3.col1.y = -this.body2.invI * this.r2.x * this.r2.y;
+        K3.col2.x = -this.body2.invI * this.r2.x * this.r2.y;
+        K3.col2.y = this.body2.invI * this.r2.x * this.r2.x;
+
+        const K = Mat22.add(Mat22.add(K1, K2), K3);
+        K.col1.x += this.softness;
+        K.col2.y += this.softness;
+
+        this.M = K.invert();
+
+        const p1 = Vec2.add(this.body1.position, this.r1);
+        const p2 = Vec2.add(this.body2.position, this.r2);
+        const dp = Vec2.sub(p2, p1);
+
+        if (positionCorrection) {
+            this.bias = Vec2.scale(-this.biasFactor * invDt, dp);
+        } else {
+            this.bias.set(0.0, 0.0);
+        }
+
+        if (warmStarting) {
+            // Apply accumulated impulse.
+            this.body1.velocity.sub(Vec2.scale(this.body1.invMass, this.P));
+            this.body1.angularVelocity -= this.body1.invI * Vec2.cross(this.r1, this.P);
+
+            this.body2.velocity.add(Vec2.scale(this.body2.invMass, this.P));
+            this.body2.angularVelocity += this.body2.invI * Vec2.cross(this.r2, this.P);
+        } else {
+            this.P.set(0.0, 0.0);
+        }
     };
 
     applyImpulse = (): void => {
@@ -77,66 +136,3 @@ export default class Joint {
         this.P.add(impulse);
     };
 }
-
-/*
-
-void Joint::PreStep(float inv_dt)
-{
-	// Pre-compute anchors, mass matrix, and bias.
-	Mat22 Rot1(body1->rotation);
-	Mat22 Rot2(body2->rotation);
-
-	r1 = Rot1 * localAnchor1;
-	r2 = Rot2 * localAnchor2;
-
-	// deltaV = deltaV0 + K * impulse
-	// invM = [(1/m1 + 1/m2) * eye(2) - skew(r1) * invI1 * skew(r1) - skew(r2) * invI2 * skew(r2)]
-	//      = [1/m1+1/m2     0    ] + invI1 * [r1.y*r1.y -r1.x*r1.y] + invI2 * [r1.y*r1.y -r1.x*r1.y]
-	//        [    0     1/m1+1/m2]           [-r1.x*r1.y r1.x*r1.x]           [-r1.x*r1.y r1.x*r1.x]
-	Mat22 K1;
-	K1.col1.x = body1->invMass + body2->invMass;	K1.col2.x = 0.0f;
-	K1.col1.y = 0.0f;								K1.col2.y = body1->invMass + body2->invMass;
-
-	Mat22 K2;
-	K2.col1.x =  body1->invI * r1.y * r1.y;		K2.col2.x = -body1->invI * r1.x * r1.y;
-	K2.col1.y = -body1->invI * r1.x * r1.y;		K2.col2.y =  body1->invI * r1.x * r1.x;
-
-	Mat22 K3;
-	K3.col1.x =  body2->invI * r2.y * r2.y;		K3.col2.x = -body2->invI * r2.x * r2.y;
-	K3.col1.y = -body2->invI * r2.x * r2.y;		K3.col2.y =  body2->invI * r2.x * r2.x;
-
-	Mat22 K = K1 + K2 + K3;
-	K.col1.x += softness;
-	K.col2.y += softness;
-
-	M = K.Invert();
-
-	Vec2 p1 = body1->position + r1;
-	Vec2 p2 = body2->position + r2;
-	Vec2 dp = p2 - p1;
-
-	if (World::positionCorrection)
-	{
-		bias = -biasFactor * inv_dt * dp;
-	}
-	else
-	{
-		bias.Set(0.0f, 0.0f);
-	}
-
-	if (World::warmStarting)
-	{
-		// Apply accumulated impulse.
-		body1->velocity -= body1->invMass * P;
-		body1->angularVelocity -= body1->invI * Cross(r1, P);
-
-		body2->velocity += body2->invMass * P;
-		body2->angularVelocity += body2->invI * Cross(r2, P);
-	}
-	else
-	{
-		P.Set(0.0f, 0.0f);
-	}
-}
-
-*/
