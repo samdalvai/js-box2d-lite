f8b591b7272529e92d7a0f8da68bb63d41943863
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-12 22:00:00 +0100

    chore: remove unused methods in Graphics

--- src/Graphics.ts
+++ src/Graphics.ts
@@ -45,115 +45,11 @@ export default class Graphics {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     };
 
-    static drawLine = (x0: number, y0: number, x1: number, y1: number, color: string): void => {
-        this.ctx.strokeStyle = color;
-        this.ctx.beginPath();
-        this.ctx.moveTo(x0, y0);
-        this.ctx.lineTo(x1, y1);
-        this.ctx.stroke();
-    };
-
-    static drawCircle = (x: number, y: number, radius: number, angle: number, color: string): void => {
-        // Draw the circle
-        this.ctx.beginPath();
-        this.ctx.arc(x, y, radius, 0, Math.PI * 2);
-        this.ctx.strokeStyle = color;
-        this.ctx.stroke();
-
-        // Draw the line from center to circle edge at given angle
-        const endX = x + Math.cos(angle) * radius;
-        const endY = y + Math.sin(angle) * radius;
-
-        this.ctx.beginPath();
-        this.ctx.moveTo(x, y);
-        this.ctx.lineTo(endX, endY);
-        this.ctx.strokeStyle = color;
-        this.ctx.stroke();
-    };
-
     static drawFillCircle = (x: number, y: number, radius: number, color: string): void => {
         this.ctx.beginPath();
         this.ctx.arc(x, y, radius, 0, Math.PI * 2);
         this.ctx.fillStyle = color;
         this.ctx.fill();
-    };
-
-    static drawRect = (x: number, y: number, width: number, height: number, color: string): void => {
-        const hw = width / 2;
-        const hh = height / 2;
-
-        this.ctx.strokeStyle = color;
-        this.ctx.beginPath();
-        this.ctx.moveTo(x - hw, y - hh);
-        this.ctx.lineTo(x + hw, y - hh);
-        this.ctx.lineTo(x + hw, y + hh);
-        this.ctx.lineTo(x - hw, y + hh);
-        this.ctx.closePath();
-        this.ctx.stroke();
-    };
-
-    static drawFillRect = (x: number, y: number, width: number, height: number, color: string): void => {
-        const hw = width / 2;
-        const hh = height / 2;
-
-        this.ctx.fillStyle = color;
-        this.ctx.fillRect(x - hw, y - hh, width, height);
-    };
-
-    static drawPolygon = (x: number, y: number, vertices: Vec2[], color: string): void => {
-        this.ctx.strokeStyle = color;
-        this.ctx.beginPath();
-
-        if (vertices.length > 0) {
-            this.ctx.moveTo(vertices[0].x, vertices[0].y);
-            for (let i = 1; i < vertices.length; i++) {
-                this.ctx.lineTo(vertices[i].x, vertices[i].y);
-            }
-            this.ctx.closePath();
-        }
-
-        this.ctx.stroke();
-
-        // draw the 1px center point like filledCircleColor(..., radius=1)
-        this.ctx.fillStyle = color;
-        this.ctx.beginPath();
-        this.ctx.arc(x, y, 1, 0, Math.PI * 2);
-        this.ctx.fill();
-    };
-
-    static drawFillPolygon = (x: number, y: number, vertices: Vec2[], color: string): void => {
-        if (vertices.length === 0) return;
-
-        // Fill polygon
-        this.ctx.fillStyle = color;
-        this.ctx.beginPath();
-        this.ctx.moveTo(vertices[0].x, vertices[0].y);
-        for (let i = 1; i < vertices.length; i++) {
-            this.ctx.lineTo(vertices[i].x, vertices[i].y);
-        }
-        this.ctx.closePath();
-        this.ctx.fill();
-
-        // Draw the 1px center point
-        this.ctx.fillStyle = '#000000'; // black like 0xFF000000 in SDL
-        this.ctx.beginPath();
-        this.ctx.arc(x, y, 1, 0, Math.PI * 2);
-        this.ctx.fill();
-    };
-
-    static drawTexture = (
-        x: number,
-        y: number,
-        width: number,
-        height: number,
-        rotation: number, // in radians
-        texture: CanvasImageSource,
-    ): void => {
-        this.ctx.save();
-        this.ctx.translate(x, y);
-        this.ctx.rotate(rotation);
-        this.ctx.drawImage(texture, -width / 2, -height / 2, width, height);
-        this.ctx.restore();
     };
 
     static drawText = (
