34c8657cbc60dc0e2018a5807194a6ee58f7fbf3
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-12 08:10:34 +0100

    feat: add implementation of clipSegmentToLine method

--- src/math/Vec2.ts
+++ src/math/Vec2.ts
@@ -7,6 +7,10 @@ export default class Vec2 {
         this.y = y;
     }
 
+    clone = (): Vec2 => {
+        return new Vec2(this.x, this.y);
+    };
+
     set = (x: number, y: number): void => {
         this.x = x;
         this.y = y;
