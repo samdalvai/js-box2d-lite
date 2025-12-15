33963b5981f40c8bb0e778cac4a153eb97c96e31
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-15 22:00:00 +0100

    refactor: convert Contact properties to Vec2

--- src/new/Contact.ts
+++ src/new/Contact.ts
@@ -3,26 +3,23 @@ import { Body } from './Body';
 import World from './World';
 
 export default class Contact {
-    body1: Body | null;
-    body2: Body | null;
-
     position: Vec2;
-    nx: number;
-    ny: number;
+    normal: Vec2;
+    r1: Vec2;
+    r2: Vec2;
+    separation: number;
     Pn: number; // accumulated normal impulse
     Pt: number; // accumulated tangent impulse
-
-    r1x: number;
-    r1y: number;
-    r2x: number;
-    r2y: number;
-    rvx: number;
-    rvy: number;
-
-    separation: number;
     massNormal: number;
     massTangent: number;
     bias: number;
+
+    body1: Body | null;
+    body2: Body | null;
+
+    rvx: number;
+    rvy: number;
+
     friction: number;
     allowedPenetration;
     biasFactor;
@@ -32,18 +29,13 @@ export default class Contact {
         this.body1 = null;
         this.body2 = null;
 
-        // this.px = 0.0;
-        // this.py = 0.0;
         this.position = new Vec2();
-        this.nx = 0.0;
-        this.ny = 0.0;
+        this.normal = new Vec2();
         this.Pn = 0.0; // accumulated normal impulse
         this.Pt = 0.0; // accumulated tangent impulse
 
-        this.r1x = 0.0;
-        this.r1y = 0.0;
-        this.r2x = 0.0;
-        this.r2y = 0.0;
+        this.r1 = new Vec2();
+        this.r2 = new Vec2();
         this.rvx = 0.0;
         this.rvy = 0.0;
 
@@ -61,8 +53,8 @@ export default class Contact {
         this.body1 = bA;
         this.body2 = bB;
         this.separation = separation;
-        this.nx = nx;
-        this.ny = ny;
+        this.normal.x = nx;
+        this.normal.y = ny;
         this.Pn = 0.0;
         this.Pt = 0.0;
         this.friction = friction;
@@ -71,28 +63,27 @@ export default class Contact {
         this.position.x = px;
         this.position.y = py;
 
-        this.r1x = this.position.x - this.body1.position.x;
-        this.r1y = this.position.y - this.body1.position.y;
-        this.r2x = this.position.x - this.body2.position.x;
-        this.r2y = this.position.y - this.body2.position.y;
+        // Relative positions
+        this.r1 = Vec2.sub(this.position, this.body1.position);
+        this.r2 = Vec2.sub(this.position, this.body2.position);
 
         // Precompute normal mass, tangent mass, and bias.
-        const rn1 = this.r1x * this.nx + this.r1y * this.ny;
-        const rn2 = this.r2x * this.nx + this.r2y * this.ny;
+        const rn1 = Vec2.dot(this.r1, this.normal);
+        const rn2 = Vec2.dot(this.r2, this.normal);
         this.massNormal =
             1.0 /
             (this.body1.invMass +
                 this.body2.invMass +
-                this.body1.invI * (this.r1x * this.r1x + this.r1y * this.r1y - rn1 * rn1) +
-                this.body2.invI * (this.r2x * this.r2x + this.r2y * this.r2y - rn2 * rn2));
-        const rt1 = this.r1x * this.ny - this.r1y * this.nx;
-        const rt2 = this.r2x * this.ny - this.r2y * this.nx;
+                this.body1.invI * (this.r1.x * this.r1.x + this.r1.y * this.r1.y - rn1 * rn1) +
+                this.body2.invI * (this.r2.x * this.r2.x + this.r2.y * this.r2.y - rn2 * rn2));
+        const rt1 = this.r1.x * this.normal.y - this.r1.y * this.normal.x;
+        const rt2 = this.r2.x * this.normal.y - this.r2.y * this.normal.x;
         this.massTangent =
             1.0 /
             (this.body1.invMass +
                 this.body2.invMass +
-                this.body1.invI * (this.r1x * this.r1x + this.r1y * this.r1y - rt1 * rt1) +
-                this.body2.invI * (this.r2x * this.r2x + this.r2y * this.r2y - rt2 * rt2));
+                this.body1.invI * (this.r1.x * this.r1.x + this.r1.y * this.r1.y - rt1 * rt1) +
+                this.body2.invI * (this.r2.x * this.r2.x + this.r2.y * this.r2.y - rt2 * rt2));
         this.bias = this.biasFactor * Math.min(0.0, this.separation + this.allowedPenetration);
     }
 
@@ -103,14 +94,14 @@ export default class Contact {
 
         this.rvx =
             this.body2.velocity.x +
-            -this.body2.angularVelocity * this.r2y -
+            -this.body2.angularVelocity * this.r2.y -
             this.body1.velocity.x -
-            -this.body1.angularVelocity * this.r1y;
+            -this.body1.angularVelocity * this.r1.y;
         this.rvy =
             this.body2.velocity.y +
-            this.body2.angularVelocity * this.r2x -
+            this.body2.angularVelocity * this.r2.x -
             this.body1.velocity.y -
-            this.body1.angularVelocity * this.r1x;
+            this.body1.angularVelocity * this.r1.x;
     }
 
     impulse(px: number, py: number) {
@@ -120,10 +111,10 @@ export default class Contact {
 
         this.body1.velocity.x -= this.body1.invMass * px;
         this.body1.velocity.y -= this.body1.invMass * py;
-        this.body1.angularVelocity -= this.body1.invI * (this.r1x * py - this.r1y * px);
+        this.body1.angularVelocity -= this.body1.invI * (this.r1.x * py - this.r1.y * px);
         this.body2.velocity.x += this.body2.invMass * px;
         this.body2.velocity.y += this.body2.invMass * py;
-        this.body2.angularVelocity += this.body2.invI * (this.r2x * py - this.r2y * px);
+        this.body2.angularVelocity += this.body2.invI * (this.r2.x * py - this.r2.y * px);
     }
 
     applyImpulse() {
@@ -131,16 +122,16 @@ export default class Contact {
         // Relative velocity at contact
         this.relativeVelocity();
         // Compute normal impulse
-        dPn = this.massNormal * (-(this.rvx * this.nx + this.rvy * this.ny) + this.bias);
+        dPn = this.massNormal * (-(this.rvx * this.normal.x + this.rvy * this.normal.y) + this.bias);
         // Clamp the accumulated impulse
         Pn0 = this.Pn;
         this.Pn = Math.max(Pn0 + dPn, 0.0);
         dPn = this.Pn - Pn0;
         // Apply contact impulse
-        this.impulse(this.nx * dPn, this.ny * dPn);
+        this.impulse(this.normal.x * dPn, this.normal.y * dPn);
         // Relative velocity at contact
         this.relativeVelocity();
-        dPn = -this.massTangent * (this.rvx * this.ny - this.rvy * this.nx);
+        dPn = -this.massTangent * (this.rvx * this.normal.y - this.rvy * this.normal.x);
         // Compute friction impulse
         const maxPt = this.friction * this.Pn;
         // Clamp friction
@@ -148,7 +139,7 @@ export default class Contact {
         this.Pt = Math.max(-maxPt, Math.min(Pn0 + dPn, maxPt));
         dPn = this.Pt - Pn0;
         // Apply contact impulse
-        this.impulse(this.ny * dPn, -this.nx * dPn);
+        this.impulse(this.normal.y * dPn, -this.normal.x * dPn);
     }
 
     draw() {
