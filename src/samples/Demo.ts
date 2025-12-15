471166aac2e60e0fe794f85328b088200f4e6dc4
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-15 22:00:00 +0100

    feat: add demo 7 to physics simulation

--- src/samples/Demo.ts
+++ src/samples/Demo.ts
@@ -69,4 +69,111 @@ export default class Demo {
             world.add(box);
         }
     };
+
+    static demo7 = (world: World) => {
+        // Demo 7: A suspension bridge
+        const floor = new Body();
+        floor.set(new Vec2(100, 20), Number.MAX_VALUE);
+        floor.friction = 0.2;
+        floor.position.set(0, -0.8 * floor.width.y);
+        floor.rotation = 0;
+        world.add(floor);
+
+        const numPlanks = 15;
+        const mass = 50;
+
+        for (let i = 0; i < numPlanks; ++i) {
+            const b = new Body();
+            b.set(new Vec2(1.0, 0.25), mass);
+            b.friction = 0.2;
+            b.position.set(-8.5 + 1.25 * i, 1.0);
+            world.add(b);
+        }
+
+        // Tuning
+        const frequencyHz = 2.0;
+        const dampingRatio = 0.7;
+
+        // frequency in radians
+        const omega = 2.0 * Math.PI * frequencyHz;
+
+        // damping coefficient
+        const d = 2.0 * mass * dampingRatio * omega;
+
+        // spring stifness
+        const k = mass * omega * omega;
+
+        // magic formulas
+        const timeStep = 1 / 50;
+        const softness = 1.0 / (d + timeStep * k);
+        const biasFactor = (timeStep * k) / (d + timeStep * k);
+
+        for (let i = 0; i < numPlanks; ++i) {
+            const j = new Joint();
+            j.set(world.bodies[i], world.bodies[i + 1], new Vec2(-9.125 + 1.25 * i, 1.0));
+            j.softness = softness;
+            j.biasFactor = biasFactor;
+
+            world.add(j);
+        }
+
+        const j = new Joint();
+        j.set(world.bodies[numPlanks], world.bodies[0], new Vec2(-9.125 + 1.25 * numPlanks, 1.0));
+        j.softness = softness;
+        j.biasFactor = biasFactor;
+        world.add(j);
+    };
 }
+
+/*
+b.Set(Vec2(100.0, 20.0), FLT_MAX);
+	b.friction = 0.2f;
+	b.position.Set(0.0, -0.5f * b.width.y);
+	b.rotation = 0.0;
+	world.Add(b);
+	++b; ++numBodies;
+
+	const int numPlanks = 15;
+	const mass = 50.0;
+
+	for (int i = 0; i < numPlanks; ++i)
+	{
+		b.Set(Vec2(1.0, 0.25f), mass);
+		b.friction = 0.2f;
+		b.position.Set(-8.5f + 1.25f * i, 5.0);
+		world.Add(b);
+		++b; ++numBodies;
+	}
+
+	// Tuning
+	const frequencyHz = 2.0;
+	const dampingRatio = 0.7f;
+
+	// frequency in radians
+	const omega = 2.0 * k_pi * frequencyHz;
+
+	// damping coefficient
+	const d = 2.0 * mass * dampingRatio * omega;
+
+	// spring stifness
+	const k = mass * omega * omega;
+
+	// magic formulas
+	const softness = 1.0 / (d + timeStep * k);
+	const biasFactor = timeStep * k / (d + timeStep * k);
+
+	for (int i = 0; i < numPlanks; ++i)
+	{
+		j.Set(bodies+i, bodies+i+1, Vec2(-9.125f + 1.25f * i, 5.0));
+		j.softness = softness;
+		j.biasFactor = biasFactor;
+
+		world.Add(j);
+		++j; ++numJoints;
+	}
+
+	j.Set(bodies + numPlanks, bodies, Vec2(-9.125f + 1.25f * numPlanks, 5.0));
+	j.softness = softness;
+	j.biasFactor = biasFactor;
+	world.Add(j);
+	++j; ++numJoints;*/
