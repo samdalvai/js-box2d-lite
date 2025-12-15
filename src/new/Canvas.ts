f311c6db7628ac44a7d12cc760cf06cb4659fc98
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-15 07:57:32 +0100

    refactor: convert Canvas class to Typescript

--- src/new/Canvas.ts
+++ src/new/Canvas.ts
@@ -1,12 +1,35 @@
 export default class Canvas {
-    constructor(resx, resy) {
+    elem: HTMLCanvasElement;
+    resx: number;
+    resy: number;
+    left: number;
+    top: number;
+
+    ctx: CanvasRenderingContext2D;
+
+    width: number;
+    height: number;
+
+    constructor(resx: number, resy: number) {
         this.elem = document.createElement('canvas');
         this.resx = resx;
         this.resy = resy;
         this.left = 0;
         this.top = 0;
+        this.width = 0;
+        this.height = 0;
 
-        this.ctx = this.elem.getContext('2d');
+        if (!this.elem) {
+            throw new Error('Could not initialize canvas element');
+        }
+
+        const ctx = this.elem.getContext('2d');
+
+        if (!ctx) {
+            throw new Error('Could not initialize context element');
+        }
+
+        this.ctx = ctx;
         document.body.appendChild(this.elem);
         this.resize();
         window.addEventListener('resize', () => this.resize(), false);
@@ -20,7 +43,7 @@ export default class Canvas {
         let o = this.elem;
         this.width = this.elem.width = this.resx;
         this.height = this.elem.height = this.resy;
-        for (this.left = 0, this.top = 0; o != null; o = o.offsetParent) {
+        for (this.left = 0, this.top = 0; o != null; o = o.offsetParent as HTMLCanvasElement) {
             this.left += o.offsetLeft;
             this.top += o.offsetTop;
         }
